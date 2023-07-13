3efd67f579489b63e8b6a814e337340329f5f86a
kamach16 <kamach.gamedev@gmail.com>
2023-07-13 16:41:32 +0200

    Add main menu

---
+++ Fortress Defender/Assets/Scripts/UI/MainMenuCanvas.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LeaderBoard()
+    {
+        // open leaderboard
+    }
+
+    public void HowToPlay()
+    {
+        // open how to play
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
